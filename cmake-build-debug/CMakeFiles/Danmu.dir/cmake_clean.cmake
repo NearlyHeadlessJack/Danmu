6f24dfe848b6857a57ee2422e5a333441745aa4e
NearlyHeadlessJack <jwang0702@foxmail.com>
2021-08-01 11:49:16 +0800

    2

---
+++ cmake-build-debug/CMakeFiles/Danmu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Danmu.dir/main.cpp.obj"
+  "Danmu.exe"
+  "Danmu.exe.manifest"
+  "Danmu.pdb"
+  "libDanmu.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Danmu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Danmu.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Danmu.
+# This may be replaced when dependencies are built.
