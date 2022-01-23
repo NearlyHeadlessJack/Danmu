5745046acd0bf34d042441b459d2aff94cdff27f
NHJWINDOWSDEKTOP <jwang0702@foxmail.com>
2022-01-23 15:19:27 +0800

    Update read.h

--- read.h
+++ read.h
@@ -6,7 +6,7 @@ class Flash
 public:
 
     Flash();
-    const char* filename = "Data-10313214-20220115.txt";
+    const char* filename = "Data-10313214-20220116.txt";
     FILE* fp = fopen(filename, "r");
 
     ~Flash()
